212e6fb412f8700205540c7eafa2b60bdbb47aec
terryjung0244 <terryjung0244@gmail.com>
2022-06-21 11:18:38 -0400

    Object state updated

--- src/components/DiaryEditor.jsx
+++ src/components/DiaryEditor.jsx
@@ -2,11 +2,34 @@ import React, { useState } from 'react'
 
 const DiaryEditor = () => {
 
-  const [auther, setAuther] = useState('');
+  const [state, setState] = useState({
+    auther: '',
+    content: ''
+  })
 
   return (
     <div>
-      <h2>Today's Diary</h2>
+      <h1>
+        Today's Diary
+      </h1>
+      <div>
+        <input 
+          name='auther'
+          placeholder='Let me hear your story'
+          value={state.auther}
+          onChange={(e) => {
+            setState({...state, auther: e.target.value})
+          }}
+        />
+      </div>
+      <div>
+        <textarea
+          value={state.content}
+          onChange={(e) => {
+            setState({...state, content: e.target.value})
+          }}
+        />
+      </div>
     </div>
   )
 }
